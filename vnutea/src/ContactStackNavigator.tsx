9b384d420a5821cc1ee331d06b20bfd4c40c239d
nghia <21020365@vnu.edu.vn>
2023-11-13 23:35:49 +0700

    u

--- vnutea/src/ContactStackNavigator.tsx
+++ vnutea/src/ContactStackNavigator.tsx
@@ -1,43 +1,43 @@
-import React from "react";
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import StudentScreen from './StudentHome/StudentScreen';
-import HomeScreen from './StudentHome/HomeScreen';
-import chat from './StudentHome/chat/chat';
-import messanger from "./StudentHome/chat/messanger";
-import Lecturer from "./StudentHome/Lecturer/Lecturer";
-import ContactLecturer from "./StudentHome/Lecturer/ContactLecturer";
-import Comment from "./StudentHome/Lecturer/Comment";
-const Stack = createNativeStackNavigator();
+// import React from "react";
+// import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// import StudentScreen from './StudentHome/StudentScreen';
+// import HomeScreen from './StudentHome/HomeScreen';
+// import chat from './StudentHome/chat/chat';
+// import messanger from "./StudentHome/chat/messanger";
+// import Lecturer from "./StudentHome/Lecturer/Lecturer";
+// import ContactLecturer from "./StudentHome/Lecturer/ContactLecturer";
+// import Comment from "./StudentHome/Lecturer/Comment";
+// const Stack = createNativeStackNavigator();
 
-const HomeStackNavigator = () => {
-  return (
-    <Stack.Navigator
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: '#77C9A3',
-        },
-        headerTintColor: '#fff',
-        headerTitleStyle: {
-          fontWeight: 'bold',
-        },
-      }}
-    >
-      <Stack.Screen name="Home" component={HomeScreen} options={{title: '',}}/>
-      <Stack.Screen name="Chat" component={chat} />
-      <Stack.Screen name="Messanger" component={messanger} />
-      <Stack.Screen name="Lecturer" component={Lecturer} />
-      <Stack.Screen name="ContactLecturer" component={ContactLecturer} />
-      <Stack.Screen name="Comment" component={Comment} />
-    </Stack.Navigator>
-  );
-}
+// const HomeStackNavigator = () => {
+//   return (
+//     <Stack.Navigator
+//       screenOptions={{
+//         headerStyle: {
+//           backgroundColor: '#77C9A3',
+//         },
+//         headerTintColor: '#fff',
+//         headerTitleStyle: {
+//           fontWeight: 'bold',
+//         },
+//       }}
+//     >
+//       <Stack.Screen name="Home" component={HomeScreen} options={{title: '',}}/>
+//       <Stack.Screen name="Chat" component={chat} />
+//       <Stack.Screen name="Messanger" component={messanger} />
+//       <Stack.Screen name="Lecturer" component={Lecturer} />
+//       <Stack.Screen name="ContactLecturer" component={ContactLecturer} />
+//       <Stack.Screen name="Comment" component={Comment} />
+//     </Stack.Navigator>
+//   );
+// }
 
-const StudentStackNavigator = () => {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen name="Student" component={StudentScreen} />
-    </Stack.Navigator>
-  );
-}
+// const StudentStackNavigator = () => {
+//   return (
+//     <Stack.Navigator>
+//       <Stack.Screen name="Student" component={StudentScreen} />
+//     </Stack.Navigator>
+//   );
+// }
 
-export { HomeStackNavigator, StudentStackNavigator };
+// export { HomeStackNavigator, StudentStackNavigator };
