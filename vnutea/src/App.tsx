ff1ad2e9e5e6480bbb58bdc2418f0cc74532992a
Pham Minh <mhp12092003@gmail.com>
2023-12-16 14:54:41 +0700

    update App.tsx

--- vnutea/src/App.tsx
+++ vnutea/src/App.tsx
@@ -1,17 +1,22 @@
-import React, { useEffect } from 'react';
-import { CompositeScreenProps, NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import React, {useEffect} from 'react';
+import {
+  CompositeScreenProps,
+  NavigationContainer,
+} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { RootStackParamList, StudentTabParamList } from './types/routing';
-import { HomeStackNavigator } from './ContactStackNavigator';
+import {RootStackParamList, StudentTabParamList} from './types/routing';
+import {HomeStackNavigator} from './ContactStackNavigator';
 import SplashScreen from 'react-native-splash-screen';
 import Onboarding from './screens/Onboarding';
 import Login from './screens/Login';
 import Icon from 'react-native-vector-icons/Octicons';
-import { SearchStackNavigator } from './screens/Search';
-import { ChatStackNavigator } from './screens/NewChat';
-import { ProfileNavigator } from './screens/FavTeacher';
+import IconFeather from 'react-native-vector-icons/Feather';
+import IconIonicons from 'react-native-vector-icons/Ionicons';
+import {SearchStackNavigator} from './screens/Search';
+import {ChatStackNavigator} from './screens/NewChat';
+import {ProfileNavigator} from './screens/FavTeacher';
 import Profile from './screens/Profile';
 import Settings from './screens/Settings';
 
@@ -25,7 +30,7 @@ function MainRootApp() {
         name="Home"
         component={HomeStackNavigator}
         options={{
-          tabBarIcon: () => <Icon name="home" size={16} color="#19253D" />,
+          tabBarIcon: () => <Icon name="home" size={24} color="#19253D" />,
         }}
       />
 
@@ -33,7 +38,21 @@ function MainRootApp() {
         name="Search"
         component={SearchStackNavigator}
         options={{
-          tabBarIcon: () => <Icon name="search" size={16} color="#19253D" />,
+          tabBarIcon: () => <Icon name="search" size={24} color="#19253D" />,
+        }}
+      />
+
+      <TabApp.Screen
+        name="Chat"
+        component={ChatStackNavigator}
+        options={{
+          tabBarIcon: () => (
+            <IconIonicons
+              name="chatbox-ellipses-outline"
+              size={28}
+              color="#19253D"
+            />
+          ),
         }}
       />
 
@@ -41,23 +60,9 @@ function MainRootApp() {
         name="Profile"
         component={ProfileNavigator}
         options={{
-          tabBarIcon: () => <Icon name="rocket" size={16} color="#19253D" />,
-        }}
-      /> 
-
-      <TabApp.Screen
-        name="Chat"
-        component={ChatStackNavigator}
-        options={{
-          tabBarIcon: () => <Icon name="rocket" size={16} color="#19253D" />,
-        }}
-      />
-
-      <TabApp.Screen
-        name="Setting"
-        component={Settings}
-        options={{
-          tabBarIcon: () => <Icon name="rocket" size={16} color="#19253D" />,
+          tabBarIcon: () => (
+            <IconFeather name="user" size={28} color="#19253D" />
+          ),
         }}
       />
     </TabApp.Navigator>
@@ -65,7 +70,9 @@ function MainRootApp() {
 }
 
 function App(): JSX.Element {
-  const [isFirstLaunched, setFirstLaunched] = React.useState<boolean | null>(null);
+  const [isFirstLaunched, setFirstLaunched] = React.useState<boolean | null>(
+    null,
+  );
 
   useEffect(() => {
     SplashScreen.hide();
@@ -86,7 +93,7 @@ function App(): JSX.Element {
 
   return isFirstLaunched !== null ? (
     <NavigationContainer>
-      <RootStack.Navigator screenOptions={{ headerShown: false }}>
+      <RootStack.Navigator screenOptions={{headerShown: false}}>
         {isFirstLaunched && (
           <RootStack.Screen name="OnBoarding" component={Onboarding} />
         )}
